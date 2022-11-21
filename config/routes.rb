f5bff8eecbc3e272c2e36e5efb4d0f226aef717b
evans ochieng <evansochieng121@gmail.com>
2022-11-21 14:55:21 +0300

    practice dynamic routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # display all cheeses
   get '/cheeses', to: 'cheeses#index'
+
+  #display individual cheese
+  get '/cheeses/:id', to: 'cheeses#show'
+
+  #find by name and display
+  get '/cheeses/display/:name', to: 'cheeses#display'
 end
